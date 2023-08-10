a674eeea2fd1d9478e607245531f38ddf2a876f5
Overloque <poludnicyn.99@mail.ru>
2023-08-10 22:53:27 +0500

    fixed dragNDropTest

--- src/test/java/githubDragnDrop/GithubDragTest.java
+++ src/test/java/githubDragnDrop/GithubDragTest.java
@@ -10,22 +10,33 @@ import org.junit.jupiter.api.Test;
 public class GithubDragTest {
     @Test
     void gitCheckPage() {
+
         //open main github page
         open("https://github.com/");
+
         //hover Solutions menu
         $(".header-menu-wrapper").$(byText("Solutions")).hover();
+
         //Choose Solutions -> Enterprize point
         $(".header-menu-wrapper").$(byText("Enterprise")).click();
+
         //Check loaded page (title - "Build like the best.")
         $(".enterprise-hero h1").shouldHave(text("Build like the best"));
     }
 
     @Test
     void dragNDropTest() {
+
         // open https://the-internet.herokuapp.com/drag_and_drop
         open("https://the-internet.herokuapp.com/drag_and_drop");
+
+        // check rectangles' positions before
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
+
         // replace rectangle A to B
         $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
+
         // check replaced rectangle's positions
         $("#column-b").shouldHave(text("A"));
         $("#column-a").shouldHave(text("B"));
@@ -35,6 +46,8 @@ public class GithubDragTest {
     @Test
     void dragNDropActionsTest() {
         open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("B"));
         actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).release().perform();
         $("#column-b").shouldHave(text("A"));
         $("#column-a").shouldHave(text("B"));
